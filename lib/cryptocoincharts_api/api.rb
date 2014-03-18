ebf8ae98ab0320af5d76aeaa3d95180374e6af61
TM Lee <tm89lee@gmail.com>
2014-03-18 14:15:08 +0800

    Add trading_pair/pairs API + test

--- lib/cryptocoincharts_api/api.rb
+++ lib/cryptocoincharts_api/api.rb
@@ -5,5 +5,15 @@ module CryptocoinchartsApi
       api_call "listCoins", options
     end
 
+    def trading_pair(options={})
+      currency_1 = options.delete(:currency_1)
+      currency_2 = options.delete(:currency_2)
+      api_call "tradingPair/#{currency_1}_#{currency_2}", options
+    end
+
+    def trading_pairs(pairs)
+      api_call "tradingPairs", pairs, :post
+    end
+
   end
 end
--- test/cryptocoincharts_api_test.rb
+++ test/cryptocoincharts_api_test.rb
@@ -16,4 +16,18 @@ class CryptocoinchartsApiTest < MiniTest::Unit::TestCase
     assert_equal result.size, 265
   end
 
+  def test_trading_pair
+    stub_get("http://www.cryptocoincharts.info/v2/api/tradingPair/drk_btc", 'trading_pair.json')
+    result = @client.trading_pair currency_1: 'drk', currency_2: 'btc'
+    assert_equal result[:id], "drk/btc"
+    assert_equal result[:price], "0.00122751"
+    assert_equal result[:best_market], "cryptsy"
+  end
+
+  def test_trading_pairs
+    stub_post("http://www.cryptocoincharts.info/v2/api/tradingPairs", 'trading_pairs.json')
+    result = @client.trading_pairs "ltc_btc,drk_btc,ppc_btc,doge_btc"
+    assert_equal result.size, 10
+  end
+
 end
